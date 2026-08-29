f8ab416a163141c066d87183a8d215b9872aa67f
mishter99 <63767992+mishter99@users.noreply.github.com>
2026-08-29 13:21:02 +0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+// sw.js
+const CACHE_NAME = 'ham-music-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Biarkan request stream & API lewat jaringan langsung
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
